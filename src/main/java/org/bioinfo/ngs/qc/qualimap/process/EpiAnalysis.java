c8ae1a142e6e0ce4d3be702c7eb37bf234127b1d
okko <okonechnikov@mpiib-berlin.mpg.de>
2012-07-18 15:44:45 +0200

    Adding GPLv2 license

--- src/main/java/org/bioinfo/ngs/qc/qualimap/process/EpiAnalysis.java
+++ src/main/java/org/bioinfo/ngs/qc/qualimap/process/EpiAnalysis.java
@@ -1,3 +1,23 @@
+/**
+ * QualiMap: evaluation of next generation sequencing alignment data
+ * Copyright (C) 2012 Garcia-Alcalde et al.
+ * http://qualimap.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
 package org.bioinfo.ngs.qc.qualimap.process;
 
 import org.bioinfo.formats.core.feature.Gff;
@@ -29,8 +49,6 @@ public class EpiAnalysis {
     static final String TAG_PARAMETERS = "parameters";
     static final String TAG_EXP_ID = "expID";
     static final String TAG_REGIONS = "regions";
-    static final String TAG_FILE = "file";
-    static final String TAG_COLUMN = "column";
     static final String TAG_LOCATION = "location";
     static final String TAG_UP = "up";
     static final String TAG_DOWN = "down";
