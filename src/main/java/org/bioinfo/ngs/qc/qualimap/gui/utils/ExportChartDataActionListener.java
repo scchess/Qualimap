ad704cc6f19a77e8b32a86a74df6f2f716549b2d
kokonech <okonechnikov@mpiib-berlin.mpg.de>
2014-03-05 17:08:46 +0100

    Update license year and version to 0.8

--- src/main/java/org/bioinfo/ngs/qc/qualimap/gui/utils/ExportChartDataActionListener.java
+++ src/main/java/org/bioinfo/ngs/qc/qualimap/gui/utils/ExportChartDataActionListener.java
@@ -1,6 +1,6 @@
 /**
  * QualiMap: evaluation of next generation sequencing alignment data
- * Copyright (C) 2013 Garcia-Alcalde et al.
+ * Copyright (C) 2014 Garcia-Alcalde et al.
  * http://qualimap.org
  *
  * This program is free software; you can redistribute it and/or
