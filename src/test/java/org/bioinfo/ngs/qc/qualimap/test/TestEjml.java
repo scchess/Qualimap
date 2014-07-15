4a17c44c3306013bea8d8425b752009eda930e3c
kokonech <okonechnikov@mpiib-berlin.mpg.de>
2014-07-15 16:37:06 +0200

    Add test to PCA implementation

--- src/test/java/org/bioinfo/ngs/qc/qualimap/test/TestEjml.java
+++ src/test/java/org/bioinfo/ngs/qc/qualimap/test/TestEjml.java
@@ -17,21 +17,46 @@ public class TestEjml {
     public void simpleTest() {
         double [] data = {1,2.2,3.3,4};
         SimpleMatrix m = new SimpleMatrix(2,2,true, data);
-        System.out.println(m.get(1,1));
         assertTrue(m.get(1,1) == 4);
 
     }
 
     @Test
     public void testPCA() {
-        double[] s1 = {1,2,3};
-        double[] s2 = {1,2,3};
+
+        double[] x = {0.69, -1.31,  0.39, 0.09,  1.29,  0.49,  0.19, -0.81, -0.31, -0.71 };
+        double[] y = {0.49, -1.21,  0.99, 0.29,  1.09,  0.79, -0.31, -0.81, -0.31, -1.01 };
+
         PrincipleComponentAnalysis pca = new PrincipleComponentAnalysis();
-        pca.addSample(s1);
-        pca.addSample(s2);
+        pca.setup(10, 2);
+
+        for (int i = 0; i < 10; ++i) {
+            double[] sample = {x[i],y[i]};
+            pca.addSample( sample );
+        }
+
         pca.computeBasis(2);
-        double[] vec = pca.getBasisVector(1);
-        System.out.println(vec[0]);
+        double[] pc1 = pca.getBasisVector(0);
+        assertTrue(pc1.length == 2);
+        System.out.println(pc1[0]);
+        System.out.println(pc1[1]);
+
+
+        /*for (int i = 0; i < 10; ++i) {
+            double[] sample = {x[i],y[i]};
+            double[] cS = pca.sampleToEigenSpace(sample);
+            System.out.println("(" + cS[0] + " " + cS[1] + ")");
+        } */
+
+        double[] s1 = {x[0],y[0]};
+        double[] c1 = pca.sampleToEigenSpace(s1);
+
+        // Expecting (-0.8279702,  0.1751153)
+        double c1Len = c1[0]*c1[0] + c1[1]*c1[1];
+        assertTrue( c1Len  - 0.7162 < 0.00001);
+
+
+
 
 
     }
